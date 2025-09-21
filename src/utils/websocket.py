2a6282f35e4b47bc8afb04305cdb3a3c2a64993e
Gloshayden <Gloshayden@gmail.com>
2025-09-21 20:50:05 +0100

    made a websocket class for easy communication

---
+++ src/utils/websocket.py
@@ -0,0 +1,34 @@
+import websocket
+
+class WebSocketClient:
+    def __init__(self, uri):
+        self.uri = uri
+        self.ws = None
+
+    def connect(self):
+        self.ws = websocket.WebSocket()
+        try:
+            self.ws.connect(self.uri)
+        except Exception as e:
+            print(f"Failed to connect to {self.uri}: {e}")
+            self.ws = None
+        
+    def respond(self, message):
+        if self.ws is not None:
+            self.ws.send(message)
+            return self.ws.recv()
+        return None
+
+    def send(self, message):
+        if self.ws is not None:
+            self.ws.send(message)
+
+    def receive(self):
+        if self.ws is not None:
+            return self.ws.recv()
+
+    def close(self):
+        if self.ws is not None:
+            self.ws.close()
+            self.ws = None
+        return
